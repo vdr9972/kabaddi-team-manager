3aa7c8f828c005fd727daddbea7de83ce9bf5f5e
vdr9972 <vdr9972@gmail.com>
2026-09-09 12:37:03 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAfAspr2KAA5mb_3M3IgzFmxxSCYRgDINo",
+  authDomain: "kabaddi-team-manager.firebaseapp.com",
+  projectId: "kabaddi-team-manager",
+  storageBucket: "kabaddi-team-manager.firebasestorage.app",
+  messagingSenderId: "94560369705",
+  appId: "1:94560369705:web:cc5272077ff346bc85f038"
+};
